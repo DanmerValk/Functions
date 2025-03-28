663cfe7e824a60e8fda22f0590f8b80041d13ebf
DanmerValk <danmervalk@gmail.com>
2025-03-29 01:08:39 +0300

    ff

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,21 +1,41 @@
 #include<iostream>
 using namespace std;
-
+void Fillrand(int arr[], const int n);
+void Print(int arr[], const int n);
+void Sort(int arr[], const int n);
 void main()
 {
 	setlocale(LC_ALL, "ru");
 	const int n = 5;
 	int arr[n];
+	Fillrand(arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+	const int SIZE = 8;
+	int brr[SIZE];
+	Fillrand(brr, SIZE);
+	Print(brr, SIZE);
+	Sort(brr, SIZE);
+	Print(brr, SIZE);
+}
+void Fillrand(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
 	}
+}
+void Print(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
-
+}
+void Sort(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = i + 1; j < n; j++)
@@ -28,37 +48,4 @@ void main()
 			}
 		}
 	}
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-	const int SIZE = 8;
-	int brr[SIZE];
-	for (int i = 0; i < SIZE; i++)
-	{
-		brr[i] = rand() % 100;
-	}
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << brr[i] << "\t";
-	}
-	cout << endl;
-	for (int i = 0; i < SIZE; i++)
-	{
-		for (int j = i + 1; j < SIZE; j++)
-		{
-			if (brr[j] < brr[i])
-			{
-				int buffer = brr[i];
-				brr[i] = brr[j];
-				brr[j] = buffer;
-			}
-		}
-	}
-	for (int i = 0; i < SIZE; i++)
-	 {
-		cout << brr[i] << "\t";
-	 }
-	cout << endl;
 }
