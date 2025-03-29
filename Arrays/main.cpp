693d9bb6fe5df43ac95c4356fd71fcb051d4cd9f
DanmerValk <danmervalk@gmail.com>
2025-03-29 11:26:36 +0300

    encode#2

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,19 +1,35 @@
 #include<iostream>
 using namespace std;
+
 void Fillrand(int arr[], const int n);
+void Fillrand(double arr[], const int n);
+
 void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+
 void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+
 int Sum(int arr[], const int n);
+int Sum(double arr[], const int n);
+
 double Avg(int arr[], const int n);
+double Avg(double arr[], const int n);
+
 int MinValue(int arr[], const int n);
+double MinValue(double arr[], const int n);
+
 int MaxValue(int arr[], const int n);
+double MaxValue(double arr[], const int n);
+
 void ShiftLeft(int arr[], const int n, const int shifts);
-void ShiftRigth(int arr[], const int n, const int shifts);
+void ShiftLeft(double arr[], const int n, const int shifts);
+
 
 void main()
 {
 	setlocale(LC_ALL, "ru");
-	const int n = 8;
+	const int n = 5;
 	int arr[n];
 	int shifts;
 	Fillrand(arr, n);
@@ -33,7 +49,7 @@ void main()
 
 
 	const int SIZE = 8;
-	int brr[SIZE];
+	double brr[SIZE];
     //int shifts;
 	Fillrand(brr, SIZE);
 	Print(brr, SIZE);
@@ -58,7 +74,22 @@ void Fillrand(int arr[], const int n)
 		arr[i] = rand() % 100;
 	}
 }
+void Fillrand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
 void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(double arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -82,6 +113,22 @@ void Sort(int arr[], const int n)
 	}
 	
 }
+void Sort(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+
+}
 int Sum(int arr[], const int n)
 {
 	int Sum = 0;
@@ -91,11 +138,33 @@ int Sum(int arr[], const int n)
     }
 	return Sum;
 }
+int Sum(double arr[], const int n)
+{
+	int Sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		Sum += arr[i];
+	}
+	return Sum;
+}
 double Avg(int arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
 }
+double Avg(double arr[], const int n)
+{
+	return (double)Sum(arr, n) / n;
+}
 int MinValue(int arr[], const int n)
+{
+	int min = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	return min;
+}
+double MinValue(double arr[], const int n)
 {
 	int min = arr[0];
 	for (int i = 1; i < n; i++)
@@ -113,12 +182,21 @@ int MaxValue(int arr[], const int n)
 	}
 	return max;
 }
+double MaxValue(double arr[], const int n)
+{
+	int max = arr[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	return max;
+}
 void ShiftLeft(int arr[], const int n, const int shifts)
 {
 	for (int i = 0; i < shifts; i++)
 	{
 		int buffer = arr[0];
-		for (int i = 0; i < n; i++)
+		for (int i = 1; i < n; i++)
 		{
 			arr[i - 1] = arr[i];
 		}
@@ -126,15 +204,16 @@ void ShiftLeft(int arr[], const int n, const int shifts)
 	}
 	
 }
-void ShiftRigth(int arr[], const int n, const int shifts)
+void ShiftLeft(double arr[], const int n, const int shifts)
 {
 	for (int i = 0; i < shifts; i++)
 	{
 		int buffer = arr[0];
-		for (int i = 0; i < n; i++)
+		for (int i = 1; i < n; i++)
 		{
-			arr[i + 1] = arr[i];
+			arr[i - 1] = arr[i];
 		}
-		arr[n + 1] = buffer;
+		arr[n - 1] = buffer;
 	}
+
 }
